8f88033e8035fef3d636ad4d029a9472c2e3dc90
sumathisiri <mekalasiri16@gmail.com>
2023-03-10 00:08:17 +0530

    first commit

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from app.views import *
+from django.urls import path
+app_name='hello'
+
+
+urlpatterns=[
+    path('suresh/',suresh,name='suresh'),
+
+]
---
+++ siri/urls.py
@@ -0,0 +1,9 @@
+from siri.views import *
+from django.urls import path
+app_name='hiii'
+
+
+
+urlpatterns=[
+    path('sai/',sai,name='sai'),
+]
